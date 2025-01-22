94d6186e8dd5c71bf3070624914c8d0917781587
dharunr36 <dharunr.23aid@kongu.edu>
2025-01-22 23:52:32 +0530

    time card.js changed

--- frontend/public/js/timecard.js
+++ frontend/public/js/timecard.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         try {
-            const response = await fetch(`http://localhost:5000/api/auth/getdata?userId=${userId}`, {
+            const response = await fetch(`https://geolocation-based-attendence-1.onrender.com/api/auth/getdata?userId=${userId}`, {
                 method: 'GET',
                 headers: {
                     "Content-Type": "application/json"
