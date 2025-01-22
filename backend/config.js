37f18f466848c1c0b001eae880e9a64c5eb7c0a2
dharunr36 <dharunr.23aid@kongu.edu>
2025-01-22 19:29:06 +0530

    Server connected

--- backend/config.js
+++ backend/config.js
@@ -3,4 +3,5 @@ require('dotenv').config(); // Load environment variables from .env file
 module.exports = {
   mongoURI: process.env.MONGO_DB_URI, // MongoDB connection URI from .env file
   port: process.env.PORT || 3000, // Port for your server
+  jwtSecret: process.env.JWT_SECRET,
 };
