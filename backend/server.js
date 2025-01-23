7317ac5760d60c391e9252c1c6faa2f834bcb040
dharunr36 <dharunr.23aid@kongu.edu>
2025-01-23 09:16:39 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const NODE_ENV = process.env.NODE_ENV || "development";
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:5500",
-  "https://geoattend-iq4uwj4ok-dharuns-projects-7c3d278a.vercel.app" // Add more origins if necessary
+  "https://geoattend-iq4uwj4ok-dharuns-projects-7c3d278a.vercel.app","https://geolocation-based-attendence-l7qj.vercel.app/" // Add more origins if necessary
 ];
 app.use(
   cors({
